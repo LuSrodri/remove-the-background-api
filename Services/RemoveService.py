4170fd50126284caa91cf2a9ecfc878dfe7a8341
Lucas Santos Rodrigues <lucassrodri08@gmail.com>
2024-02-03 21:41:10 -0300

    Improve background remove process

--- Services/RemoveService.py
+++ Services/RemoveService.py
@@ -1,12 +1,14 @@
 import cv2
-from rembg import remove
+from rembg import remove, new_session
 
 def remove_background(image_path):
     image_name = image_path.split('\\')[-1]
     output_image_path = './uploads\\' + image_name.split('.')[0] + '_no_bg.png'
 
     input = cv2.imread(image_path)
-    output = remove(input)
+    model_name = "silueta"
+    session = new_session(model_name)
+    output = remove(input, session=session, post_process_mask=True)
     cv2.imwrite(output_image_path, output)
 
     return output_image_path
